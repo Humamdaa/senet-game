ba079d44036dfca871ef1fc4f3f99c1bd6034c12
Humamdaa <homamdaas11@gmail.com>
2026-01-09 20:57:43 +0300

    apply clean architecture and implement Human vs Human mode

---
+++ modes/HumanVsHuman.py
@@ -0,0 +1,29 @@
+from utils.RandomStep import Random
+from game.Board import Board
+from game.Rules import Rules
+from game.Render import Render
+
+
+class HumanVsHuman:
+    def __init__(self, board):
+        self.board: Board = board
+        self.rand = Random()
+
+    def start(self):
+        Render.draw_board(self.board.grid)
+
+        while True:
+            dist = self.rand.roll_distance()
+
+            while True:
+                print('\n', '='*50,
+                      f"\n {self.board.get_current_player()} can move {dist} cells")
+
+                cur_pos = int(input('select the piece to move : '))
+
+                if Rules.checkMove(self.board, cur_pos - 1, dist):
+                    Rules.move(self.board, cur_pos - 1, dist)
+                    Render.draw_board(self.board.grid)
+                    self.board.switchPlayer()
+                    break
+                print('try again my dear')
